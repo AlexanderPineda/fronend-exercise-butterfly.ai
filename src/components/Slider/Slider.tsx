94eba8cae58cc5875af600a9588a7061a8f9508e
deBuenasDev <correopiineda@gmail.com>
2023-12-13 19:12:43 -0500

    fix(component): fix slider behavior in mobile devices

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -24,7 +24,7 @@ const Slider: React.FC<SliderProps> = ({
     return () => {
       window.removeEventListener("resize", handleWindowSizeChange);
     };
-  }, []);
+  }, [mobile]);
 
   useEffect(() => {
     const dots = Array.from(document.querySelectorAll(".rc-slider-dot"));
